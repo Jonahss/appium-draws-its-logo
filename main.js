856656bfcf69359b0b8865f70c961b0ccdad3b3b
Jonah Stiennon <jonahss@gmail.com>
2014-06-19 16:08:14 -0700

    parabolic radian reduction, not quite touching

--- main.js
+++ main.js
@@ -68,18 +68,18 @@ var innerArcAction = function(start, end) {
   var sin = Math.sin;
   var theta = start;
 
-  var iterations = 0;
+  var step = 4*pi/100;
+  var min_radius = 30;
 
   var prev = {x: h+r*cos(theta), y: k+r*sin(theta)}
   var action = new TouchAction(driver);
   action.press(prev);
-  for (; theta < end; theta+=2*pi/100) {
-    var rad = r - (theta-start)
+  for (; theta < end; theta+=step) {
+    var rad = 7.5991*Math.pow(theta-start-pi, 2)+25;
     var next = {x: h+rad*cos(theta), y: k+rad*sin(theta)}
     var offset = {x: next.x-prev.x, y: next.y-prev.y}
     action.moveTo(offset);
     prev = next;
-    iterations++;
   }
   action.release();
   return action;
@@ -90,8 +90,17 @@ var multiAction = function() {
   multi.add(
     arcAction(0, 2*pi/3*1),
     arcAction(2*pi/3*1, 2*pi/3*2),
-    arcAction(2*pi/3*2, 2*pi/3*3),
-    innerArcAction(0, 2*pi/3*1)
+    arcAction(2*pi/3*2, 2*pi/3*3)
+  );
+  return multi;
+}
+
+var innerArcMulti = function() {
+  var multi = new MultiAction();
+  multi.add(
+    innerArcAction(0, pi),
+    innerArcAction(2*pi/3*1, 2*pi/3*1 + pi),
+    innerArcAction(2*pi/3*2, 2*pi/3*2 + pi)
   );
   return multi;
 }
@@ -102,7 +111,9 @@ driver.init(caps, function() {
       driver.elementByAccessibilityId('Go', function(err, el) {
         el.click(function (){
           driver.sleep('2000', function(){
-            driver.performMultiAction(multiAction());
+            driver.performMultiAction(multiAction(), function() {
+              driver.performMultiAction(innerArcMulti());
+            });
           })
         });
       });
